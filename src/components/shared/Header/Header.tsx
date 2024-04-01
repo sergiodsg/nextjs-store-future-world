00f1f8dd3210247af92024ad52cb2c3f1c5c5524
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-04-01 14:31:07 -0400

    ✔ Second Module

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,19 +1,23 @@
-import Link from "next/link";
+import Link from 'next/link'
+import styles from './Header.module.css'
 
 export const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/store">
-            <li>Store</li>
-          </Link>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/store">
+              Store
+            </Link>
+          </li>
         </ul>
       </nav>
-    </header>
-  );
+    </header>)
 }
 
