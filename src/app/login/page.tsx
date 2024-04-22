938dca644f22b3bffed5548c0333c3d2ae99e24d
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-04-22 18:50:07 -0400

    ✔ Sixth Module

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from "app/components/login/LoginForm";
+import { LoginForm } from "app/components/login/loginForm";
 
 export default function LoginPage(){
   return <LoginForm />
